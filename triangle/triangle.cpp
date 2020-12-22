7c0b700ec1979c74c8f9793a02a782b36b6536a0
Davi Wendt <daviwendt@gmail.com>
2020-12-22 15:26:23 -0300

    Got the triangle to move in a way
    You can move the vertices of the triangle, but it doesn't behave properly

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <array>
+#include <cmath>
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
@@ -38,6 +39,33 @@ bool linkingSuccess(unsigned int shaderProgram) {
 	}
 	else return true;
 }
+
+void convertToOpenGL(double* xpos, double* ypos) {
+	// This is somewhat of a bad example, because the resolution is hard coded
+	double convertedX{ ((*xpos / 800) - 0.5) * 2 };
+	double convertedY{ ((*ypos / 600) - 0.5) * 2 };
+	*xpos = convertedX > 1.0 || convertedX < -1.0 ? 0.0 : convertedX;
+	*ypos = convertedY > 1.0 || convertedY < -1.0 ? 0.0 : convertedY;
+}
+
+void checkAndChange(float* vertsArray, int lenght, float xpos, float ypos) {
+	// This is also a bad example, since I'm hardcoding how the verts are placed in the array
+	float smallestDistance{20.0f};
+	float* smallestXpos;
+	float* smallestYpos;
+	for (int index{}; index < lenght - 1; index += 3) {
+		float compareDistance{ std::abs(std::sqrt(std::pow(xpos - vertsArray[index], 2) + std::pow(ypos - vertsArray[index + 1], 2))) };
+		if (compareDistance < smallestDistance) {
+			smallestDistance = compareDistance;
+			smallestYpos = &vertsArray[index + 1];
+			smallestXpos = &vertsArray[index];
+		}
+	}
+
+	*smallestXpos = xpos;
+	*smallestYpos = ypos;
+}
+
 
 int main() {
 	// Boilerplate to open a window
@@ -116,7 +144,7 @@ int main() {
 	constexpr char *fragmentShaderSource{ "#version 330 core\n"
 		"out vec4 FragColor;\n"
 		"void main() {\n"
-			"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+			"FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
 		"}\0"
 	};
 
@@ -153,11 +181,21 @@ int main() {
 	// Main Rendering loop
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
+		// Poll the mouse button state
+		// Cool Idea for when I figure out how to fix
+		// Currently moves the vertices in a very bad way
+		if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+			double xpos{}, ypos{};
+			glfwGetCursorPos(window, &xpos, &ypos);
+			convertToOpenGL(&xpos, &ypos);
+			checkAndChange(triangleVertices, 9, xpos, ypos);
+		}
 
 		// CLEARS THE SCREEN
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
 		// Draw the array, which is set by the VAO bound previously
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
