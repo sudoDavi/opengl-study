859c0b88d4b2e1985a63af22b5b0b10b53b07bdd
Davi Wendt <daviwendt@gmail.com>
2021-09-07 15:34:29 -0300

    Horror Lighting

--- src/lighting/multiple-lights/multiple-lights.cpp
+++ src/lighting/multiple-lights/multiple-lights.cpp
@@ -251,16 +251,16 @@ int main() {
 
 	// Directional Light
 	lightingShader.setVec3f("directionalLight.direction", lightDirection);
-	lightingShader.setVec3f("directionalLight.ambient", 0.3f, 0.24f, 0.14f);
-	lightingShader.setVec3f("directionalLight.diffuse", 0.7f, 0.42f, 0.26f);
-	lightingShader.setVec3f("directionalLight.specular", lightColor * 0.5f);
+	lightingShader.setVec3f("directionalLight.ambient", lightColor * 0.0f);
+	lightingShader.setVec3f("directionalLight.diffuse", lightColor * 0.05f);
+	lightingShader.setVec3f("directionalLight.specular", lightColor * 0.2f);
 
 	// Point Lights
 	glm::vec3 pointLightColors[]{
-		glm::vec3(1.0f, 0.6f, 0.0f),
-		glm::vec3(1.0f, 0.0f, 0.0f),
-		glm::vec3(1.0f),
-		glm::vec3(0.2f, 0.2f, 1.0f)
+		glm::vec3(0.1f),
+		glm::vec3(0.1f),
+		glm::vec3(0.1f),
+		glm::vec3(0.3f, 0.2f, 1.0f)
 	};
 
 	for (auto index{0}; index < 4; ++index) {
@@ -338,7 +338,7 @@ int main() {
 		projection = glm::perspective(glm::radians(camera.Zoom()), 800.0f / 600.0f, 0.1f, 100.0f);
 
 		// CLEARS THE SCREEN
-		glClearColor(0.75f, 0.52f, 0.3f, 1.0f);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Draw a set of Cubes (Containers)
