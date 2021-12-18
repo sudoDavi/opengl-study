785b8e4583e9685d30b2970841342eca388f472f
Davi Wendt <daviwendt@gmail.com>
2021-12-17 23:24:24 -0300

    Preparations for Bloom chapter

---
+++ src/lighting/bloom/bloom.cpp
@@ -0,0 +1,341 @@
+#include "constants.hpp"
+#include "shader.hpp"
+#include "texture.hpp"
+#include "flycam.hpp"
+#include "framebuffer.hpp"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stb_image.h>
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+
+// I know it is a global variable, but I'm just using it because I haven't implemented a Game Object yet
+// and it's just so I can use the scrool for the zoom
+FlyCam camera{ glm::vec3(0.0f, 0.0f, 3.0f) };
+
+// HDR Exposure
+float exposure{ 1.0f };
+// HDR Enable
+bool EnableHDR{ true };
+
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+	static bool HDRKeyPressed{ false };
+
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS) {
+		exposure += 0.001f;
+		std::cout << "exposure: " << exposure << std::endl;
+	}
+	if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS) {	
+		exposure -= 0.001f;
+		std::cout << "exposure: " << exposure << std::endl;
+	}
+	if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS && !HDRKeyPressed) {
+		HDRKeyPressed = true;
+		EnableHDR = !EnableHDR;
+		std::cout << "HDR: " << EnableHDR << std::endl;
+	}
+	if (glfwGetKey(window, GLFW_KEY_T) == GLFW_RELEASE)
+		HDRKeyPressed = false;
+}
+
+float getVisibility(GLFWwindow *window) {
+	static float visibility{ 0.2f };
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+		visibility += 0.05f;
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+		visibility -= 0.05f;
+
+	return visibility;
+}
+
+bool shouldRotate(GLFWwindow *window) {
+	static bool rotating{ false };
+	static int delay{};
+	++delay;
+	if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS && delay > 10) {
+		rotating = !rotating;
+		delay = 0;
+	}
+
+	return rotating;
+}
+
+
+
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+	static bool wireframeMode{ false };
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+		wireframeMode = !wireframeMode;
+
+	if (wireframeMode) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		std::cout << "Set to wireframe\n";
+	}
+	else {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		std::cout << "Set to fill\n";
+	}
+}
+
+void movement(GLFWwindow* window, float deltaTime, FlyCam& camera) {
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.Move(Camera::Movement::FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+       camera.Move(Camera::Movement::BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.Move(Camera::Movement::LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.Move(Camera::Movement::RIGHT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+		camera.Move(Camera::Movement::UP, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+		camera.Move(Camera::Movement::DOWN, deltaTime);
+}
+
+void mouseDirection(GLFWwindow* window, FlyCam& camera) {
+	static float lastX{ 400.0f }, lastY{ 300.0f };
+	double xpos{}, ypos{};
+	glfwGetCursorPos(window, &xpos, &ypos);
+
+	float xoffset{ xpos - lastX };
+	float yoffset{ lastY - ypos };
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.PointAt(xoffset, yoffset);
+}
+
+void scrollHandle(GLFWwindow* window, double xoffset, double yoffset) {
+	std::cout << yoffset << '\n';
+	camera.ChangeZoom(yoffset);
+}
+
+int main() {
+	// Boilerplate to open a window
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	// Added for Mac OS X
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	GLFWwindow* window{ glfwCreateWindow(800, 600, "LearnOpenGL Tutorials", nullptr, nullptr) };
+	if (window == nullptr) {
+		std::cout << "Failed to create GLFW window\n" ;
+		glfwTerminate();
+		return 1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD\n";
+		glfwTerminate();
+		return 1;
+	}
+	// End of Boilerplate
+
+	// Assign a viewport of size 800x600
+	glViewport(0, 0, 800, 600);
+	// Assign a callback function for when the screen is resized
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	// CUBE vertices for the light object
+	float cubeVertices[] = {
+	     // back face
+            -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+             1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+             1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f, // bottom-right         
+             1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+            -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+            -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f, // top-left
+            // front face
+            -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+             1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f, // bottom-right
+             1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+             1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+            -1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f, // top-left
+            -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+            // left face
+            -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+            -1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-left
+            -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+            -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+            -1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+            -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+            // right face
+             1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+             1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+             1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-right         
+             1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+             1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+             1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left     
+            // bottom face
+            -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+             1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f, // top-left
+             1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+             1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+            -1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+            -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+            // top face
+            -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+             1.0f,  1.0f , 1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+             1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f, // top-right     
+             1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+            -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+            -1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f
+	};
+
+	float planeVertices[] = {
+        // positions            // normals         // texcoords
+         25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+        -25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+        -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+
+         25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+        -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+         25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,  25.0f, 25.0f
+	};
+
+        float quadVertices[] = {
+            // positions        // texture Coords
+            -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+            -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+             1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+             1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+        };
+
+	// Create a VAO for the cubes
+	unsigned int cubeVAO{};
+	glGenVertexArrays(1, &cubeVAO);
+	glBindVertexArray(cubeVAO);
+	// Create a Vertex Buffer Object for the cubes
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	// Bind the VBO to the array buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// Input the vertex data into the array buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+
+	// Vertex position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// Normal Vector attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	// UV Map attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+
+	// Plane VAO,VBO
+	std::uint32_t planeVAO, planeVBO;
+	glGenVertexArrays(1, &planeVAO);
+	glBindVertexArray(planeVAO);
+	glGenBuffers(1, &planeVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
+
+	// Vertex position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// Normal Vector attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	// UV Map attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	// setup quad VAO
+	std::uint32_t quadVAO, quadVBO;
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+
+	// Load Textures
+
+	// Shaders
+	
+	// Bind a GLFW callback to change the drawing mode
+	glfwSetMouseButtonCallback(window, mouseButtonCallback);
+
+	// Capture the mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// Bind callback to Scroll Input
+	glfwSetScrollCallback(window, scrollHandle);
+
+
+	// Enabling Depth Testing
+	glEnable(GL_DEPTH_TEST);
+	// Enabling Culling
+	//glEnable(GL_CULL_FACE);
+
+
+	float deltaTime{};
+	float lastFrame{ glfwGetTime() };
+
+	bool hidePointLight{ false };
+
+	// Main Rendering loop
+	while (!glfwWindowShouldClose(window)) {
+		float currentFrame{ glfwGetTime() };
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		// Input handling
+		processInput(window);
+
+		// Update Transforms
+
+		glm::mat4 view{ glm::mat4(1.0f) };
+
+	        movement(window, deltaTime, camera);
+        	mouseDirection(window, camera);
+	        view = camera.GetLookAt();
+		
+		glm::mat4 projection{ glm::mat4(1.0f) };
+		
+		projection = glm::perspective(glm::radians(camera.Zoom()), 800.0f / 600.0f, 0.1f, 100.0f);
+		glm::mat4 model{ glm::mat4(1.0f) };
+	
+		// CLEARS THE SCREEN
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// TODO: Add Bloom example code
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
